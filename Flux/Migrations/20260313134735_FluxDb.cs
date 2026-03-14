a383b0aeca0f51a7cfe5fe95d7b647339dada375
Phinnf <bnm915915@gmail.com>
2026-03-14 11:31:16 +0700

    Add reply

---
+++ Flux/Migrations/20260313134735_FluxDb.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Flux.Migrations
+{
+    /// <inheritdoc />
+    public partial class FluxDb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
